e7ceec42a8efbff00e7cc4db661736aa2caa2e55
Samapa1 <saija.m.pajari@gmail.com>
2024-05-20 15:21:45 +0300

    osa3 backend versio3

--- index.js
+++ index.js
@@ -109,11 +109,6 @@ app.post('/api/persons', (request, response) => {
     response.json(numberData)
   })
 
-// const PORT = 3001
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`)
-// })
-
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
